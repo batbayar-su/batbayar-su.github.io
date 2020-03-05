daf5af37c1fed5a4371a435815d3eed507827ec6
Batbayar Sukhbaatar <batbayar.sukhbaatar@grabtaxi.com>
2020-03-05 16:20:27 +0800

    static folder with icons and custom header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,6 @@
-const pages = [
-  { label: "home", value: "home" },
-  { label: "profile", value: "profile" },
-  { label: "experience", value: "experience" },
-  { label: "exducation", value: "exducation" },
-  { label: "projects", value: "projects" },
-  { label: "skills", value: "skills" },
-  { label: "contact", value: "contact" },
-];
+const pages = {
+  home: "/"
+};
 
 const logo = document.getElementsByClassName("logo")[0];
 const input = document.getElementById("path_input");
@@ -14,4 +8,34 @@ const input = document.getElementById("path_input");
 logo.onclick = () => {
   input.focus();
   input.select();
-}
+};
+
+// Prevent backspace browser navigation
+document.onkeydown = function(event) {
+  if (!event) { // This will happen in IE
+    event = window.event;
+  }
+
+  if (
+    event.keyCode == 8 &&
+    (event.target || event.srcElement).tagName != "TEXTAREA" &&
+    (event.target || event.srcElement).tagName != "INPUT"
+  ) {
+    if (navigator.userAgent.toLowerCase().indexOf("msie") == -1) {
+      event.stopPropagation();
+    } else {
+      event.returnValue = false;
+    }
+
+    return false;
+  }
+};
+
+input.onkeypress = (event) => {
+  if (event.keyCode == 13 || event.which == 13) {
+    console.log(pages)
+    const page = pages[input.value] ? pages[input.value] : input.value
+    console.log(page)
+    window.location.href = `/${page}`;
+  }
+};
