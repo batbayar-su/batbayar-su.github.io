1e333e6840b16c524ce60c5217b2929043fcdb13
Batbayar Sukhbaatar <batbayar.sukhbaatar@grabtaxi.com>
2020-03-05 16:18:56 +0800

    removed autocompletejs and refactors

--- assets/js/stretchy.js
+++ assets/js/stretchy.js
@@ -4,216 +4,234 @@
  * MIT license
  */
 (function() {
+  if (!self.Element) {
+    return; // super old browser
+  }
 
-if (!self.Element) {
-	return; // super old browser
-}
+  if (!Element.prototype.matches) {
+    Element.prototype.matches =
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector ||
+      Element.prototype.oMatchesSelector ||
+      null;
+  }
 
-if (!Element.prototype.matches) {
-	Element.prototype.matches = Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector || Element.prototype.oMatchesSelector || null;
-}
+  if (!Element.prototype.matches) {
+    return;
+  }
 
-if (!Element.prototype.matches) {
-	return;
-}
+  function $$(expr, con) {
+    return expr instanceof Node || expr instanceof Window
+      ? [expr]
+      : [].slice.call(
+          typeof expr == "string"
+            ? (con || document).querySelectorAll(expr)
+            : expr || []
+        );
+  }
 
-function $$(expr, con) {
-	return expr instanceof Node || expr instanceof Window? [expr] :
-	       [].slice.call(typeof expr == "string"? (con || document).querySelectorAll(expr) : expr || []);
-}
+  var _ = (self.Stretchy = {
+    selectors: {
+      base:
+        'textarea, select:not([size]), input:not([type]), input[type="' +
+        "text number url email tel".split(" ").join('"], input[type="') +
+        '"]',
+      filter: ".stretchy, .stretchy *"
+    },
 
-var _ = self.Stretchy = {
-	selectors: {
-		base: 'textarea, select:not([size]), input:not([type]), input[type="' + "text number url email tel".split(" ").join('"], input[type="') + '"]',
-    filter: ".stretchy, .stretchy *"
-	},
+    // Script element this was included with, if any
+    script: document.currentScript || $$("script").pop(),
 
-	// Script element this was included with, if any
-	script: document.currentScript || $$("script").pop(),
+    // Autosize one element. The core of Stretchy.
+    resize: function(element) {
+      if (!_.resizes(element)) {
+        return;
+      }
 
-	// Autosize one element. The core of Stretchy.
-	resize: function(element) {
-		if (!_.resizes(element)) {
-			return;
-		}
+      var cs = getComputedStyle(element);
+      var offset = 0;
+      var empty;
 
-		var cs = getComputedStyle(element);
-		var offset = 0;
-		var empty;
+      if (!element.value && element.placeholder) {
+        empty = true;
+        element.value = element.placeholder;
+      }
 
-		if (!element.value && element.placeholder) {
-			empty = true;
-			element.value = element.placeholder;
-		}
+      var type = element.nodeName.toLowerCase();
 
-		var type = element.nodeName.toLowerCase();
+      if (type == "textarea") {
+        element.style.height = "0";
 
-		if (type == "textarea") {
-			element.style.height = "0";
+        if (cs.boxSizing == "border-box") {
+          offset = element.offsetHeight;
+        } else if (cs.boxSizing == "content-box") {
+          offset = -element.clientHeight + parseFloat(cs.minHeight);
+        }
 
-			if (cs.boxSizing == "border-box") {
-				offset = element.offsetHeight;
-			}
-			else if (cs.boxSizing == "content-box") {
-				offset = -element.clientHeight + parseFloat(cs.minHeight);
-			}
+        element.style.height = element.scrollHeight + offset + "px";
+      } else if (type == "input") {
+        // First test that it is actually visible, otherwise all measurements are off
+        element.style.width = "1000px";
 
-			element.style.height = element.scrollHeight + offset + "px";
-		}
-		else if (type == "input") {
-			// First test that it is actually visible, otherwise all measurements are off
-			element.style.width = "1000px";
+        if (element.offsetWidth) {
+          element.style.width = "0";
 
-			if (element.offsetWidth) {
-				element.style.width = "0";
+          if (cs.boxSizing == "border-box") {
+            offset = element.offsetWidth;
+          } else if (cs.boxSizing == "padding-box") {
+            offset = element.clientWidth;
+          } else if (cs.boxSizing == "content-box") {
+            offset = parseFloat(cs.minWidth);
+          }
 
-				if (cs.boxSizing == "border-box") {
-					offset = element.offsetWidth;
-				}
-				else if (cs.boxSizing == "padding-box") {
-					offset = element.clientWidth;
-				}
-				else if (cs.boxSizing == "content-box") {
-					offset = parseFloat(cs.minWidth);
-				}
+          var width = Math.max(
+            offset,
+            element.scrollWidth - element.clientWidth
+          );
 
-				var width = Math.max(offset, element.scrollWidth - element.clientWidth);
+          element.style.width = width + "px";
 
-				element.style.width = width + "px";
+          // To bulletproof, we will set scrollLeft to a
+          // huge number, and read that back to see what it was clipped to
+          // and increment width by that much, iteratively
 
-				// To bulletproof, we will set scrollLeft to a
-				// huge number, and read that back to see what it was clipped to
-				// and increment width by that much, iteratively
+          for (var i = 0; i < 10; i++) {
+            // max iterations
+            element.scrollLeft = 1e10;
 
-				for (var i=0; i<10; i++) { // max iterations
-					element.scrollLeft = 1e+10;
+            if (element.scrollLeft == 0) {
+              break;
+            }
 
-					if (element.scrollLeft == 0) {
-						break;
-					}
+            width += element.scrollLeft;
 
-					width += element.scrollLeft;
+            element.style.width = width + "px";
+          }
+        } else {
+          // Element is invisible, just set to something reasonable
+          element.style.width = element.value.length + 1 + "ch";
+        }
+      } else if (type == "select") {
+        var selectedIndex =
+          element.selectedIndex > 0 ? element.selectedIndex : 0;
 
-					element.style.width = width + "px";
-				}
-			}
-			else {
-				// Element is invisible, just set to something reasonable
-				element.style.width = element.value.length + 1 + "ch";
-			}
-		}
-		else if (type == "select") {
-			var selectedIndex = element.selectedIndex > 0? element.selectedIndex : 0;
+        // Need to use dummy element to measure :(
+        var option = document.createElement("_");
+        option.textContent = element.options[selectedIndex].textContent;
+        element.parentNode.insertBefore(option, element.nextSibling);
 
-			// Need to use dummy element to measure :(
-			var option = document.createElement("_");
-			option.textContent = element.options[selectedIndex].textContent;
-			element.parentNode.insertBefore(option, element.nextSibling);
+        // The name of the appearance property, as it might be prefixed
+        var appearance;
 
-			// The name of the appearance property, as it might be prefixed
-			var appearance;
+        for (var property in cs) {
+          var value = cs[property];
+          if (
+            !/^(width|webkitLogicalWidth|length)$/.test(property) &&
+            typeof value == "string"
+          ) {
+            option.style[property] = value;
 
-			for (var property in cs) {
-				var value = cs[property];
-				if (!/^(width|webkitLogicalWidth|length)$/.test(property) && typeof value == "string") {
-					option.style[property] = value;
+            if (/appearance$/i.test(property)) {
+              appearance = property;
+            }
+          }
+        }
 
-					if (/appearance$/i.test(property)) {
-						appearance = property;
-					}
-				}
-			}
+        option.style.width = "";
 
-			option.style.width = "";
+        if (option.offsetWidth > 0) {
+          element.style.width = option.offsetWidth + "px";
 
-			if (option.offsetWidth > 0) {
-				element.style.width = option.offsetWidth + "px";
+          if (!cs[appearance] || cs[appearance] !== "none") {
+            // Account for arrow
+            element.style.width = "calc(" + element.style.width + " + 2em)";
+          }
+        }
 
-				if (!cs[appearance] || cs[appearance] !== "none") {
-					// Account for arrow
-					element.style.width = "calc(" + element.style.width + " + 2em)";
-				}
-			}
+        option.parentNode.removeChild(option);
+        option = null;
+      }
 
-			option.parentNode.removeChild(option);
-			option = null;
-		}
+      if (empty) {
+        element.value = "";
+      }
+    },
 
-		if (empty) {
-			element.value = "";
-		}
-	},
+    // Autosize multiple elements
+    resizeAll: function(elements) {
+      $$(elements || _.selectors.base).forEach(function(element) {
+        _.resize(element);
+      });
+    },
 
-	// Autosize multiple elements
-	resizeAll: function(elements) {
-		$$(elements || _.selectors.base).forEach(function (element) {
-			_.resize(element);
-		});
-	},
+    active: true,
 
-	active: true,
+    // Will stretchy do anything for this element?
+    resizes: function(element) {
+      return (
+        element &&
+        element.parentNode &&
+        element.matches &&
+        element.matches(_.selectors.base) &&
+        element.matches(_.selectors.filter)
+      );
+    },
 
-	// Will stretchy do anything for this element?
-	resizes: function(element) {
-		return element &&
-		       element.parentNode &&
-		       element.matches &&
-		       element.matches(_.selectors.base) &&
-		       element.matches(_.selectors.filter);
-	},
+    init: function() {
+      _.selectors.filter =
+        _.script.getAttribute("data-filter") ||
+        ($$("[data-stretchy-filter]").pop() || document.body).getAttribute(
+          "data-stretchy-filter"
+        ) ||
+        _.selectors.filter;
 
-	init: function(){
-		_.selectors.filter = _.script.getAttribute("data-filter") ||
-		                     ($$("[data-stretchy-filter]").pop() || document.body).getAttribute("data-stretchy-filter") || _.selectors.filter;
+      _.resizeAll();
 
-		_.resizeAll();
+      // Listen for new elements
+      if (self.MutationObserver && !_.observer) {
+        _.observer = new MutationObserver(function(mutations) {
+          if (_.active) {
+            mutations.forEach(function(mutation) {
+              if (mutation.type == "childList") {
+                _.resizeAll(mutation.addedNodes);
+              }
+            });
+          }
+        });
+        _.observer.observe(document.documentElement, {
+          childList: true,
+          subtree: true
+        });
+      }
+    },
 
-		// Listen for new elements
-		if (self.MutationObserver && !_.observer) {
-			_.observer = new MutationObserver(function(mutations) {
-				if (_.active) {
-					mutations.forEach(function(mutation) {
-						if (mutation.type == "childList") {
-							_.resizeAll(mutation.addedNodes);
-						}
-					});
-				}
-			});
-			_.observer.observe(document.documentElement, {
-				childList: true,
-				subtree: true
-			});
-		}
-	},
+    $$: $$
+  });
 
-	$$: $$
-};
+  // Autosize all elements once the DOM is loaded
 
-// Autosize all elements once the DOM is loaded
+  // DOM already loaded?
+  if (document.readyState !== "loading") {
+    requestAnimationFrame(_.init);
+  } else {
+    // Wait for it
+    document.addEventListener("DOMContentLoaded", _.init);
+  }
 
-// DOM already loaded?
-if (document.readyState !== "loading") {
-	requestAnimationFrame(_.init);
-}
-else {
-	// Wait for it
-	document.addEventListener("DOMContentLoaded", _.init);
-}
+  window.addEventListener("load", function() {
+    _.resizeAll();
+  });
 
-window.addEventListener("load", function(){
-	_.resizeAll();
-});
+  // Listen for changes
+  var listener = function(evt) {
+    if (_.active) {
+      _.resize(evt.target);
+    }
+  };
 
-// Listen for changes
-var listener = function(evt) {
-	if (_.active) {
-		_.resize(evt.target);
-	}
-};
+  document.documentElement.addEventListener("input", listener);
 
-document.documentElement.addEventListener("input", listener);
-
-// Firefox fires a change event instead of an input event
-document.documentElement.addEventListener("change", listener);
-
+  // Firefox fires a change event instead of an input event
+  document.documentElement.addEventListener("change", listener);
 })();
