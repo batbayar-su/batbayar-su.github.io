effb8416e1f3f6d660dc21c9cc21ba21bd256e8e
Batbayar Sukhbaatar <batbayar.sukhbaatar@sap.com>
2022-10-16 18:45:52 +0200

    setup nextui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+const theme = createTheme({
+  type: 'dark', // it could be "light" or "dark"
+  theme: {
+    colors: {
+      gradient:
+        'linear-gradient(90deg, $blue600 -25%, $blue800 -10%, $purple700 80%)',
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
